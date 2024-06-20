73700210f072250367bb482b905385be2f9f4f8c
김나연 <126762806+Nangniya@users.noreply.github.com>
2024-06-20 11:42:02 +0900

    [Bronze II] Title: 분해합, Time: 120 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -1,7 +1,7 @@
 const input = require("fs").readFileSync(0).toString().trim();
 const N = Number(input);
 let answer = 0;
-for (let i = 1; i <= N; i++) {
+for (let i = N - input.length * 9; i <= N; i++) {
   const arr = String(i).split("").map(Number);
   const M = i + arr.reduce((acc, cur) => acc + cur, 0);
   if (M === N) {
