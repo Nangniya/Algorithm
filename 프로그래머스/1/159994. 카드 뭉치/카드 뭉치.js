c4cf023aeec0510bbd59f114f7d89746a5618abf
김나연 <126762806+Nangniya@users.noreply.github.com>
2024-09-04 17:35:08 +0900

    [level 1] Title: 카드 뭉치, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,10 @@
 function solution(cards1, cards2, goal) {
     const queue = [];
+    let one = 0;
+    let two = 0;
     for(let i = 0; i < goal.length; i++){
-        if(cards1[0] === goal[i]) queue.push(cards1.shift());
-        if(cards2[0] === goal[i]) queue.push(cards2.shift());
+        if(cards1[one] === goal[i]) queue.push(cards1[one++])
+        if(cards2[two] === goal[i]) queue.push(cards2[two++]);
     }
     return goal.join('') === queue.join('') ? "Yes" : "No"
 }
