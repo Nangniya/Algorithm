a2fc278950c3a9d06b8c44fff0d147be76c1ab6d
김나연 <126762806+Nangniya@users.noreply.github.com>
2024-10-31 17:00:43 +0900

    [level 3] Title: 네트워크, Time: 1.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,20 @@
+function dfs(computers, visited, node) {
+    visited[node] = true;
+    for (let i = 0; i < computers[node].length; i++) {
+        if(computers[node][i] && !visited[i]) {
+            dfs(computers, visited, i);
+        }
+    }
+}
+
+function solution(n, computers) {
+    let answer = 0;
+    const visited = Array(n).fill(false);
+    for (let i = 0; i < n; i++) {
+        if(!visited[i]) {
+            dfs(computers, visited, i);
+            answer++;
+        }
+    }
+    return answer;
+}
