f44b375455235caf30b8b1f525a6b4a4c4bc1efd
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-01-05 21:07:15 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -1,8 +1,8 @@
 const input = require("fs").readFileSync(0).toString().trim();
 const N = Number(input);
 
-for (let i = 1; i < 2 * N; i++) {
-  if (i < N) console.log(" ".repeat(N - i) + "*".repeat(2 * i - 1));
-  else if (i === N) console.log("*".repeat(2 * N - 1));
-  else console.log(" ".repeat(i - N) + "*".repeat(2 * N - 2 * (i - N) - 1));
+for (let i = 0; i < N*2-1; i++) {
+    const spaces = Math.abs(N - i - 1);
+    const stars = 2 * (N - spaces) - 1;
+    console.log(" ".repeat(spaces) + "*".repeat(stars));
 }
