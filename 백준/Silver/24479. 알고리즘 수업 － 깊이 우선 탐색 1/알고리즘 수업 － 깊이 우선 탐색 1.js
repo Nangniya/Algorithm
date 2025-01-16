aab0fde067e453c8314c382c3c1902d0f436f686
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-01-16 14:42:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 752 ms, Memory: 94420 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -0,0 +1,38 @@
+const input = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+const [N, M, R] = input[0].split(" ").map(Number);
+
+const graph = Array.from({ length: N + 1 }, () => []);
+
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(" ").map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
+}
+
+for (let i = 1; i <= N; i++) {
+  graph[i].sort((a, b) => a - b);
+}
+
+const visited = new Array(N + 1).fill(0);
+let order = 1; 
+
+function dfs(node) {
+  visited[node] = order++;
+
+  for (const next of graph[node]) {
+    if (visited[next] === 0) {
+      // 아직 방문하지 않은 정점이면
+      dfs(next);
+    }
+  }
+}
+
+dfs(R);
+
+console.log(visited.slice(1).join("\n"));
