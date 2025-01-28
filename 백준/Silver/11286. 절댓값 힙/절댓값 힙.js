692bcde7d9e11c1e1cb288befdc86d6406f50c0b
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-01-28 13:11:14 +0900

    [Silver I] Title: 절댓값 힙, Time: 244 ms, Memory: 27676 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.js
@@ -0,0 +1,94 @@
+class AbsMinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  push(item) {
+    this.heap.push(item);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.isEmpty()) return 0;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return min;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+
+      if (
+        Math.abs(this.heap[parentIndex]) < Math.abs(this.heap[index]) ||
+        (Math.abs(this.heap[parentIndex]) === Math.abs(this.heap[index]) &&
+          this.heap[parentIndex] < this.heap[index])
+      )
+        break;
+
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+
+    while (index < this.heap.length) {
+      const leftChild = index * 2 + 1;
+      const rightChild = index * 2 + 2;
+      const smallerChild =
+        this.heap[rightChild] &&
+        (Math.abs(this.heap[rightChild]) < Math.abs(this.heap[leftChild]) ||
+          (Math.abs(this.heap[rightChild]) === Math.abs(this.heap[leftChild]) &&
+            this.heap[rightChild] < this.heap[leftChild]))
+          ? rightChild
+          : leftChild;
+
+      if (
+        !this.heap[leftChild] ||
+        Math.abs(this.heap[index]) < Math.abs(this.heap[smallerChild]) ||
+        (Math.abs(this.heap[index]) === Math.abs(this.heap[smallerChild]) &&
+          this.heap[index] < this.heap[smallerChild])
+      )
+        break;
+
+      this.swap(index, smallerChild);
+      index = smallerChild;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length <= 0;
+  }
+}
+
+const input = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const pq = new AbsMinHeap();
+const answer = [];
+
+for (let i = 1; i < input.length; i++) {
+  const V = input[i];
+  if (V === 0) {
+    answer.push(pq.pop());
+  } else {
+    pq.push(V);
+  }
+}
+
+console.log(answer.join("\n"));
