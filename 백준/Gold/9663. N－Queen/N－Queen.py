c3a4fa10ae0cf894754612bb61ff6093cf3fcc02
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-02-04 18:28:32 +0900

    [Gold IV] Title: N-Queen, Time: 19428 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -8,19 +8,19 @@ DIAGONAL_RIGHT = [False] * 2 * N;
 DIAGONAL_LEFT = [False] * 2 * N;
 
 def backtrack(y):
-   answer = 0;
+   answer = 0
    
    if y == N:
-      answer += 1;
+      answer += 1
    else:
       for i in range(N):
          if WIDTH[i] or DIAGONAL_RIGHT[i + y] or DIAGONAL_LEFT[N - i + y]:
-            continue;
+            continue
          
-         WIDTH[i] = DIAGONAL_RIGHT[i + y] = DIAGONAL_LEFT[N - i + y] = True;
-         answer += backtrack(y + 1);
-         WIDTH[i] = DIAGONAL_RIGHT[i + y] = DIAGONAL_LEFT[N - i + y] = False;
+         WIDTH[i] = DIAGONAL_RIGHT[i + y] = DIAGONAL_LEFT[N - i + y] = True
+         answer += backtrack(y + 1)
+         WIDTH[i] = DIAGONAL_RIGHT[i + y] = DIAGONAL_LEFT[N - i + y] = False
    
-   return answer;
+   return answer
 
-print(backtrack(0));
+print(backtrack(0))
