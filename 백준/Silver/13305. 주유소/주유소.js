c2fb9ed67c1e80b05d2b2c0c3cb5bf7843c70932
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-02-20 22:17:30 +0900

    [Silver III] Title: 주유소, Time: 284 ms, Memory: 44368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,18 +2,20 @@ const input = require("fs")
   .readFileSync(0)
   .toString()
   .trim()
-  .split("\n")
-  .map((v) => v.trim());
+  .split("\n");
 
 const N = Number(input[0]);
-const DIST = input[1].split(" ").map(Number);
-const COST = input[2].split(" ").map(Number);
+const DIST = input[1].split(" ").map(BigInt); // BigInt 사용
+const COST = input[2].split(" ").map(BigInt); // BigInt 사용
 
-let answer = 0;
+let answer = 0n; // BigInt 초기화
 let minCost = COST[0];
+
 for (let i = 0; i < N - 1; i++) {
-  minCost = Math.min(minCost, COST[i]);
-  answer += minCost * DIST[i];
+  if (COST[i] < minCost) {
+    minCost = COST[i]; // 더 저렴한 가격이 나오면 갱신
+  }
+  answer += minCost * DIST[i]; // 현재 최소 가격으로 거리만큼 주유
 }
 
-console.log(answer);
+console.log(answer.toString()); // BigInt는 문자열로 변환하여 출력
