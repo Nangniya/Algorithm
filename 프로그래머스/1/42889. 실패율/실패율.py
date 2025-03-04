8b032218a7af8e687c18e5a5270f96d93d1747c2
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-03-04 15:03:40 +0900

    [level 1] Title: 실패율, Time: 25.10 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,23 +1,17 @@
 def solution(N, stages):
-    passed_dp = [0] * (N + 1)
-    challenging = [0] * (N + 1)
-    ratios = [0] * (N + 1)
-    
-    for level in stages:
-        if level != N + 1:
-            challenging[level] += 1
-            passed_dp[level] += 1
-        else:
-            passed_dp[level - 1] += 1
-            
-    for i in range(N - 1, 0, -1):
-        passed_dp[i] += passed_dp[i + 1]
-        
-    for i in range(1, N + 1):
-        if passed_dp[i] == 0:
-            ratios[i] = 0
-        else:
-            ratios[i] = challenging[i] / passed_dp[i]
-        
-    result = sorted(range(1, N + 1), key=lambda x: -ratios[x])    
-    return result
+  challenger = [0] * (N + 2)
+  for stage in stages:
+    challenger[stage] += 1
+  
+  fails = { } 
+  total = len(stages) 
+  
+  for i in range(1, N + 1):
+    if challenger[i] == 0:
+      fails[i] = 0
+    else:
+      fails[i] = challenger[i] / total
+      total -= challenger[i] 
+
+  result = sorted(fails, key=lambda x : fails[x], reverse=True)
+  return result
