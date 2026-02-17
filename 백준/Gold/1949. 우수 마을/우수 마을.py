f0458ea674936f5de7ae0aa90bd19174c130934e
김나연 <126762806+Nangniya@users.noreply.github.com>
2026-02-17 19:31:53 +0900

    [Gold II] Title: 우수 마을, Time: 48 ms, Memory: 37448 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -0,0 +1,35 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    population = [0] + list(map(int, input().split()))
+
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        u, v = map(int, input().split())
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    # dp[node][0] = node가 우수 마을이 아닐 때 최댓값
+    # dp[node][1] = node가 우수 마을일 때 최댓값
+    dp = [[0, 0] for _ in range(n + 1)]
+    visited = [False] * (n + 1)
+
+    def dfs(node):
+        visited[node] = True
+        dp[node][1] = population[node]  # 우수 마을일 경우 인구 수
+
+        for neighbor in adj[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+                dp[node][0] += max(dp[neighbor][0], dp[neighbor][1])  # 우수 마을이 아닐 경우
+                dp[node][1] += dp[neighbor][0]  # 우수 마을일 경우 자식은 우수 마을이 될 수 없음
+
+    dfs(1)
+    print(max(dp[1][0], dp[1][1]))
+
+if __name__ == "__main__":
+    solve()
